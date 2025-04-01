0dc2112904ab6bb3c0cbda907849688d0f635e69
Joe Monkila <jnkiwa25@gmail.com>
2025-04-01 12:07:28 +0100

    user services

--- src/main/java/com/monkilattech/madeinrdc/models/User.java
+++ src/main/java/com/monkilattech/madeinrdc/models/User.java
@@ -23,7 +23,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
-    private String name;
+    private String username;
     private String email;
     private String password;
     private String role; 
