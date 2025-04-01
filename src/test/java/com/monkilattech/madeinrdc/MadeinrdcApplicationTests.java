b7fe300b7d6088a66ade1862134fbf7d6c80a8a3
Joe Monkila <jnkiwa25@gmail.com>
2025-04-01 10:59:18 +0100

    init commit

---
+++ src/test/java/com/monkilattech/madeinrdc/MadeinrdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkilattech.madeinrdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeinrdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
