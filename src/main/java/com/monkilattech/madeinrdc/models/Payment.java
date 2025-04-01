64383cc2c64b8d66586d7fa998b9655a68bdfeee
Joe Monkila <jnkiwa25@gmail.com>
2025-04-01 11:15:48 +0100

    payment model

---
+++ src/main/java/com/monkilattech/madeinrdc/models/Payment.java
@@ -0,0 +1,27 @@
+package com.monkilattech.madeinrdc.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String paymentMethod;
+    private double amount;
+    
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
