adb65be6d91ba923b8fa3bd141c0075e7763ac25
Joe Monkila <jnkiwa25@gmail.com>
2025-04-01 12:32:15 +0100

    fixed model and servives (jwt)

---
+++ src/main/java/com/monkilattech/madeinrdc/models/ERole.java
@@ -0,0 +1,8 @@
+package com.monkilattech.madeinrdc.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_SELLER,
+    ROLE_BUYER,
+}
--- src/main/java/com/monkilattech/madeinrdc/models/User.java
+++ src/main/java/com/monkilattech/madeinrdc/models/User.java
@@ -1,13 +1,19 @@
 package com.monkilattech.madeinrdc.models;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,7 +32,11 @@ public class User {
     private String username;
     private String email;
     private String password;
-    private String role; 
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+    
     private String profil;
     private String phone;
     private Boolean status;
