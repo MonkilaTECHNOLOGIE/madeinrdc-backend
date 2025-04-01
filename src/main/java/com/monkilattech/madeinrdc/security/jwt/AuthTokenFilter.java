5e09ab3c6cbbcb4683d11e0066c89559eda35dea
Joe Monkila <jnkiwa25@gmail.com>
2025-04-01 11:53:19 +0100

    jwt

--- src/main/java/com/monkilattech/madeinrdc/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/monkilattech/madeinrdc/security/jwt/AuthTokenFilter.java
@@ -1,62 +1,62 @@
-package com.monkilattech.madeinrdc.security.jwt;
+// package com.monkilattech.madeinrdc.security.jwt;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.FilterChain;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.web.filter.OncePerRequestFilter;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+// import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.monkilatech.propertymanagement.security.services.UserDetailsServiceImpl;
+// import com.monkilattech.madeinrdc.security.services.UserDetailsServiceImpl;
 
-public class AuthTokenFilter extends OncePerRequestFilter {
-    @Autowired
-    private JwtUtils jwtUtils;
+// public class AuthTokenFilter extends OncePerRequestFilter {
+//     @Autowired
+//     private JwtUtils jwtUtils;
 
-    @Autowired
-    private UserDetailsServiceImpl userDetailsService;
+//     @Autowired
+//     private UserDetailsServiceImpl userDetailsService;
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+//     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request,
-            HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        try {
-            String jwt = request.getHeader("Authorization");
-            if (jwt != null && jwtUtils.validateJwtToken(jwt.substring(7))) {
-                String username = jwtUtils.getUserNameFromJwtToken(jwt.substring(7));
-                logger.info("Username " + username);
+//     @Override
+//     protected void doFilterInternal(HttpServletRequest request,
+//             HttpServletResponse response, FilterChain filterChain)
+//             throws ServletException, IOException {
+//         try {
+//             String jwt = request.getHeader("Authorization");
+//             if (jwt != null && jwtUtils.validateJwtToken(jwt.substring(7))) {
+//                 String username = jwtUtils.getUserNameFromJwtToken(jwt.substring(7));
+//                 logger.info("Username " + username);
 
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+//                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities());
+//                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+//                         userDetails,
+//                         null,
+//                         userDetails.getAuthorities());
 
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        } catch (Exception e) {
-            logger.error("Cannot set user authentication: {}", e);
-        }
+//                 SecurityContextHolder.getContext().setAuthentication(authentication);
+//             }
+//         } catch (Exception e) {
+//             logger.error("Cannot set user authentication: {}", e);
+//         }
 
-        filterChain.doFilter(request, response);
-    }
+//         filterChain.doFilter(request, response);
+//     }
 
-    // private String parseJwt(HttpServletRequest request) {
-    // String jwt = jwtUtils.getJwtFromCookies(request);
-    // return jwt;
-    // }
-}
+//     // private String parseJwt(HttpServletRequest request) {
+//     // String jwt = jwtUtils.getJwtFromCookies(request);
+//     // return jwt;
+//     // }
+// }
