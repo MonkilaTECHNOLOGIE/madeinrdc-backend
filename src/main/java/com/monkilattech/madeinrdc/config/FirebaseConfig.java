43164fbe6b3ab00b8df2af3c538a8bcbd94ce991
Joe Monkila <jnkiwa25@gmail.com>
2025-04-02 09:13:22 +0100

    firebase config

--- src/main/java/com/monkilattech/madeinrdc/config/FirebaseConfig.java
+++ src/main/java/com/monkilattech/madeinrdc/config/FirebaseConfig.java
@@ -1,22 +1,22 @@
-package com.monkilattech.madeinrdc.config;
+// package com.monkilattech.madeinrdc.config;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import java.io.FileInputStream;
-import java.io.IOException;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.firebase.FirebaseApp;
+// import com.google.firebase.FirebaseOptions;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import java.io.FileInputStream;
+// import java.io.IOException;
 
-@Configuration
-public class FirebaseConfig {
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase.json");
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+// @Configuration
+// public class FirebaseConfig {
+//     @Bean
+//     public FirebaseApp firebaseApp() throws IOException {
+//         FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase.json");
+//         FirebaseOptions options = FirebaseOptions.builder()
+//                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                 .build();
 
-        return FirebaseApp.initializeApp(options);
-    }
-}
+//         return FirebaseApp.initializeApp(options);
+//     }
+// }
