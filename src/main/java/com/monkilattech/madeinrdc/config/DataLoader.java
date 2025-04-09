c954bdf3861ba57f7887ed4130e06c0a41ecf895
Joe Monkila <jnkiwa25@gmail.com>
2025-04-09 09:27:56 +0100

    data loader (config)

--- src/main/java/com/monkilattech/madeinrdc/config/DataLoader.java
+++ src/main/java/com/monkilattech/madeinrdc/config/DataLoader.java
@@ -4,11 +4,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.monkilattech.madeinrdc.models.ERole;
 import com.monkilattech.madeinrdc.models.Role;
+import com.monkilattech.madeinrdc.models.User;
 import com.monkilattech.madeinrdc.repository.RoleRepository;
+import com.monkilattech.madeinrdc.repository.UserRepository;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -16,12 +19,20 @@ public class DataLoader implements CommandLineRunner {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void run(String... args) {
         insererRoleSiNonExistant(ERole.ROLE_ADMIN);
         insererRoleSiNonExistant(ERole.ROLE_BUYER);
         insererRoleSiNonExistant(ERole.ROLE_SELLER);
         insererRoleSiNonExistant(ERole.ROLE_USER);
+
+        insererAdminParDefaut();
         
     }
 
@@ -34,6 +45,28 @@ public class DataLoader implements CommandLineRunner {
             System.out.println("Role inséré : " + nom);
         } else {
             System.out.println("Role déjà existant : " + nom);
+        }
+    }
+
+    private void insererAdminParDefaut() {
+        String username = "monkila";
+        String email = "jnkiwa25@gmail.com";
+
+        if (userRepository.findByUsername(username).isEmpty()) {
+            User admin = new User();
+            admin.setUsername(username);
+            admin.setEmail(email);
+            admin.setPassword(passwordEncoder.encode("405522")); 
+
+            Role roleAdmin = roleRepository.findByName(ERole.ROLE_ADMIN)
+                    .orElseThrow(() -> new RuntimeException("ROLE_ADMIN non trouvé"));
+
+            admin.getRoles().add(roleAdmin);
+            userRepository.save(admin);
+
+            System.out.println("Utilisateur admin créé avec succès !");
+        } else {
+            System.out.println("Admin déjà existant.");
         }
     }
 }
